cbf399c1c6a4dccd5dda73d74a51edbd1e02fb96
hairilhumsani <hairilhumsani@gmail.com>
2022-07-28 15:45:59 +0800

    added sidebar for you dani

--- src/app/webpages/admin-page/admin-page.component.ts
+++ src/app/webpages/admin-page/admin-page.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
+  events: string[] = [];
+  opened: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
