9ce10dd822b549f41f1db16a2f0d0f5c33fd5435
hairilhumsani <hairilhumsani@gmail.com>
2022-08-04 20:37:14 +0800

    commit

--- src/app/webpages/student-page/student-home/question-page/question-page.component.ts
+++ src/app/webpages/student-page/student-home/question-page/question-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Question } from 'src/app/spring/model/question';
+import { StudentService } from 'src/app/spring/service/student.service';
 
 @Component({
   selector: 'app-question-page',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionPageComponent implements OnInit {
 
-  constructor() { }
+  question: Question[]
+  getCategoryData: any;
+
+  constructor(
+
+    private route: ActivatedRoute,
+    private studentService: StudentService
+
+  ) {
+
+    this.getCategoryData = this.route.snapshot.queryParamMap.get('category')
+
+  }
 
   ngOnInit(): void {
+
+    this.getQuestionCategory()
   }
+
+  getQuestionCategory() {
+    const token = sessionStorage.getItem('token');
+    this.studentService.getQuestionsByCategory(token, this.getCategoryData).subscribe(
+      data => {
+        this.question = data
+        console.log(this.question)
+      }
+    )
+  }
+
+  
 
 }
--- src/app/webpages/student-page/student-home/student-home.component.ts
+++ src/app/webpages/student-page/student-home/student-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Question } from 'src/app/spring/model/question';
+import { StudentService } from 'src/app/spring/service/student.service';
 
 @Component({
   selector: 'app-student-home',
@@ -9,21 +11,29 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class StudentHomeComponent implements OnInit {
 
   category: string[];
+  new_category: string[];
+  question : Question[];
 
   constructor(
 
-    private router : Router,
+    private router: Router,
     private route: ActivatedRoute
 
+
   ) {
-    this. category = ['Science','Maths','History','English']
-   }
+    this.category = ['Science', 'Maths', 'History', 'English']
+  }
 
   ngOnInit(): void {
+
   }
 
-  takeTest(page: string) {
-    this.router.navigate([page], { relativeTo: this.route.parent })
+
+
+
+  takeTest(page: string, c: string) {
+    this.router.navigate([page], { relativeTo: this.route.parent, queryParams: { category: c } })
   }
+
 
 }
